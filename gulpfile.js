21dd37a845027ee1cd350ab1b436f711be3d9611
Ivan Wills <Ivan.Wills2@optus.com.au>
2015-10-18 05:27:12 +1100

    Getting the map working

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 
 var gulp     = require('gulp');
 var gulpCopy = require('gulp-copy');
+var plugins  = require('gulp-load-plugins')();
+var runSequence = require('run-sequence');
 
 gulp.task('copy-lodash', function() {
     return gulp.src([
@@ -40,3 +42,34 @@ gulp.task('copy-ractive-touch', function() {
     .pipe(gulpCopy('js', { prefix: 1 }));
 });
 gulp.task('copy', ['copy-lodash', 'copy-ractive', 'copy-ractivef', 'copy-hammerjs', 'copy-superagent', 'copy-ractive-touch']);
+
+const DEV_SERVER_PORT = 7080;
+
+gulp.task('connect', function () {
+    plugins.connect.server({
+        root: '.',
+        livereload: true,
+        port: DEV_SERVER_PORT
+    });
+});
+
+gulp.task('watch', function () {
+    var self = this;
+    plugins.watch([
+        '*.html',
+        '**/*.js',
+        '**/*.css'
+    ], function () {
+        runSequence('build', 'html', function (err) {
+            self.emit('end');
+        });
+    });
+
+});
+
+gulp.task('default', function () {
+    var self = this;
+    runSequence('copy',  'connect', 'watch', function (err) {
+        self.emit('end');
+    });
+});
--- js/map.js
+++ js/map.js
@@ -1,19 +1,29 @@
-/* globals Ractive, RactiveF, _, superagent */
+/* globals Ractive, RactiveF, _, superagent, L */
 
 (function() {
-	var options = {};
-	options.el = document.querySelector('#container');
-	options.components = {};
-	options.computed = {};
-	options.template = '#template';
-	options.data = {};
+    var options = {};
+    options.el = document.querySelector('#container');
+    options.components = {};
+    options.computed = {};
+    options.template = '#template';
+    options.data = {
+        events: true
+    };
 
-	_.extend(options.components, RactiveF.components);
-	_.extend(options.computed, {
-	});
+    _.extend(options.components, RactiveF.components);
+    _.extend(options.computed, {
+    });
 
-	var ractive = new Ractive(options);
+    var map = L.map('map').setView([-33.786, 151.121], 16);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'ivanwills.cifvcpu18233zt5lyrpbtaau4',
+        accessToken: 'pk.eyJ1IjoiaXZhbndpbGxzIiwiYSI6ImNpZnZjcHZzNTIyeTF1N2x4YmlveHgyYTMifQ.24HDwqrrCD8E_YHhCvF_kg'
+    }).addTo(map);
 
-	ractive.on('thing', function(evt) {
-	});
+    var ractive = new Ractive(options);
+
+    ractive.on('thing', function(evt) {
+    });
 })();
