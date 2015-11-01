db43cd67275433c0d4048725bd4c7a30c82965af
Ivan Wills <ivan.wills@gmail.com>
2015-11-02 07:11:02 +1100

    Updating with yo ractive-foundations

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
-var fs = require('fs');
+//var fs = require('fs');
 var app = express();
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/public/'));
 app.listen(8888);
 
 console.log('Map Blaster running on http://localhost:8888');
